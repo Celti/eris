7d70fdb9c56259f5f5c5c98a570c83ba28e96e9c
Patrick Burroughs (Celti) <celti@celti.name>
2017-07-22 17:19:56 -0700

    use match guards instead of matches on floats

--- src/commands/gurps.rs
+++ src/commands/gurps.rs
@@ -18,33 +18,32 @@ fn get_strength(st: f64) -> String {
         sw_dice  = 1.0;
     
         match st {
-            0.0       => { thr_adds = "-∞"; sw_adds = "-∞"; }
-            1.0 | 2.0 => { thr_adds = "-6"; sw_adds = "-5"; }
-            3.0 | 4.0 => { thr_adds = "-5"; sw_adds = "-4"; }
-            5.0 | 6.0 => { thr_adds = "-4"; sw_adds = "-3"; }
-            7.0 | 8.0 => { thr_adds = "-3"; sw_adds = "-2"; }
-            9.0       => { thr_adds = "-2"; sw_adds = "-1"; }
-            10.0      => { thr_adds = "-2"; sw_adds = ""; }
+            _ if st <= 2.0  => { thr_adds = "-6"; sw_adds = "-5"; }
+            _ if st <= 4.0  => { thr_adds = "-5"; sw_adds = "-4"; }
+            _ if st <= 6.0  => { thr_adds = "-4"; sw_adds = "-3"; }
+            _ if st <= 8.0  => { thr_adds = "-3"; sw_adds = "-2"; }
+            _ if st <= 9.0  => { thr_adds = "-2"; sw_adds = "-1"; }
+            _ if st <= 10.0 => { thr_adds = "-2"; sw_adds = ""; }
             _         => unreachable!()
         }
     } else {
         thr_frac = (st as f64 - 5.0)/8.0;
         thr_dice = thr_frac.floor();
         match thr_frac % 1.0 {
-            0.0  | 0.125 => { thr_adds = ""; }
-            0.25 | 0.375 => { thr_adds = "+1"; }
-            0.50 | 0.625 => { thr_adds = "+2"; }
-            0.75 | 0.875 => { thr_adds = "-1"; thr_dice = thr_dice + 1.0; },
+            x if x < 0.25 => { thr_adds = ""; }
+            x if x < 0.50 => { thr_adds = "+1"; }
+            x if x < 0.75 => { thr_adds = "+2"; }
+            x if x < 1.0  => { thr_adds = "-1"; thr_dice = thr_dice + 1.0; },
             _ => unreachable!()
         };
 
         sw_frac = (st as f64 - 6.0)/4.0;
         sw_dice = sw_frac.floor();
         match sw_frac % 1.0 {
-            0.0  | 0.125 => { sw_adds = ""; }
-            0.25 | 0.375 => { sw_adds = "+1"; }
-            0.50 | 0.625 => { sw_adds = "+2"; }
-            0.75 | 0.875 => { sw_adds = "-1"; sw_dice = sw_dice + 1.0; },
+            x if x < 0.25 => { sw_adds = ""; }
+            x if x < 0.50 => { sw_adds = "+1"; }
+            x if x < 0.75 => { sw_adds = "+2"; }
+            x if x < 1.0  => { sw_adds = "-1"; sw_dice = sw_dice + 1.0; },
             _ => unreachable!()
         };
     }
